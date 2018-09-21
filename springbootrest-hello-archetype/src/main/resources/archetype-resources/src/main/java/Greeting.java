dc8c4657046c9d6b429ee8f12f8a6e7469207a7f
Kiran Kumar <kkumar1@paypal.com>
2018-09-21 16:54:45 +0530

    initial commit - added archetype for spring boot with simple hello world spring controller

---
+++ springbootrest-hello-archetype/src/main/resources/archetype-resources/src/main/java/Greeting.java
@@ -0,0 +1,13 @@
+package $package;
+
+public class Greeting {
+    private final String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
