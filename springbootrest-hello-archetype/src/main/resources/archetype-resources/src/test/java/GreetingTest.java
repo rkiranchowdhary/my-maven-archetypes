00514ab55b4948859d3099458392876661e8368a
Kiran Kumar <kkumar1@paypal.com>
2018-09-26 06:32:16 +0530

    Fixed the pom.xml

--- springbootrest-hello-archetype/src/main/resources/archetype-resources/src/test/java/GreetingTest.java
+++ springbootrest-hello-archetype/src/main/resources/archetype-resources/src/test/java/GreetingTest.java
@@ -15,7 +15,7 @@ public class GreetingTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public GreetingTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class GreetingTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( GreetingTest.class );
     }
 
     /**
